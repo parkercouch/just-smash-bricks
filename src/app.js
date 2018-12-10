fde7d9f1f336e25f00d8efbb2ad6bf37cc2b7bb5
Parker Couch <parkercouch@gmail.com>
2018-12-10 10:26:33 -0800

    Asset loading added

--- src/app.js
+++ src/app.js
@@ -1,37 +1,54 @@
 /* eslint-disable */
 
 document.addEventListener('DOMContentLoaded', function () {
-  // console.log('DOM LOADED');
+  console.log('DOM LOADED');
+  console.log(gameStates.state);
+  gameStates.startLoading();
+  console.log(gameStates.state);
 });
 
+
+// Set kontra assets paths and assets to load at start
+kontra.assets.imagePath = './assets/img';
+kontra.assets.audioPath = './assets/sfx';
+const imgAssets = [
+  'ball.png',
+  'brick.png',
+  'paddle.png',
+];
+const sfxAssets = [
+  'hit.ogg',
+];
+
+
+
+  // ------------------------------------------------------- //
+  // -------------------STATE MANAGEMENT-------------------- //
+  // ------------------------------------------------------- //
+
+const loadAssets = function () {
+  kontra.assets.load(...imgAssets, ...sfxAssets)
+    .then(() => {
+      // all assets have loaded
+      console.log('All assets loaded');
+    }).catch((err) => {
+      // error loading an asset
+      console.log(err);
+    });
+};
+
 var gameStates = new StateMachine({
-  init: 'loading',
+  init: 'pageLoad',
   transitions: [
+    { name: 'startLoading',     from: 'pageLoad',  to: 'loading' },
     { name: 'finishLoading',     from: 'loading',  to: 'menu' },
     { name: 'start',     from: 'menu',  to: 'game' },
     { name: 'pause',   from: 'game', to: 'paused'  },
     { name: 'unpause',   from: 'paused', to: 'game'  },
     { name: 'quit', from: '*', to: 'menu'    },
-  ]
+  ],
+  methods: {
+    onLoading: loadAssets(), 
+  }
 });
 
-// console.log(gameStates.state); // loading
-// console.log(gameStates.finishLoading());
-// console.log(gameStates.state); // menu
-// console.log(gameStates.start());
-// console.log(gameStates.state); // game
-// console.log(gameStates.pause());
-// console.log(gameStates.state); // paused
-// console.log(gameStates.unpause());
-// console.log(gameStates.state); // game
-// console.log(gameStates.quit());
-// console.log(gameStates.state); // menu 
-// try {
-// console.log(gameStates.pause());
-// console.log(gameStates.state); // Will not run, there is an exception
-// } catch (err) {
-//   console.log(err);
-
-// }
-// console.log(gameStates.quit());
-// console.log(gameStates.state); // menu
