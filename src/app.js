01f90e2b6f26da32d218b142af4000dd73d9d023
Parker Couch <parkercouch@gmail.com>
2018-12-10 09:51:52 -0800

    Inital states created

--- src/app.js
+++ src/app.js
@@ -1,4 +1,37 @@
+/* eslint-disable */
+
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOM LOADED');
+  // console.log('DOM LOADED');
 });
 
+var gameStates = new StateMachine({
+  init: 'loading',
+  transitions: [
+    { name: 'finishLoading',     from: 'loading',  to: 'menu' },
+    { name: 'start',     from: 'menu',  to: 'game' },
+    { name: 'pause',   from: 'game', to: 'paused'  },
+    { name: 'unpause',   from: 'paused', to: 'game'  },
+    { name: 'quit', from: '*', to: 'menu'    },
+  ]
+});
+
+// console.log(gameStates.state); // loading
+// console.log(gameStates.finishLoading());
+// console.log(gameStates.state); // menu
+// console.log(gameStates.start());
+// console.log(gameStates.state); // game
+// console.log(gameStates.pause());
+// console.log(gameStates.state); // paused
+// console.log(gameStates.unpause());
+// console.log(gameStates.state); // game
+// console.log(gameStates.quit());
+// console.log(gameStates.state); // menu 
+// try {
+// console.log(gameStates.pause());
+// console.log(gameStates.state); // Will not run, there is an exception
+// } catch (err) {
+//   console.log(err);
+
+// }
+// console.log(gameStates.quit());
+// console.log(gameStates.state); // menu
