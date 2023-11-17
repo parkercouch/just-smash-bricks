a26bf8d6dff427200cfa05c7c8c7259f11072e4c
parker couch <code@parkercouch.dev>
2023-11-17 09:26:23 -0800

    Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:prettier/recommended',
   ],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
