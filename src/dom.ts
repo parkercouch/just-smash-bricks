305287185ef501bcc51c08ef98c5e612ecca5f08
parker couch <code@parkercouch.dev>
2023-12-21 00:04:41 -0800

    minor update to ball collision

--- src/dom.ts
+++ src/dom.ts
@@ -56,11 +56,11 @@ export function initMuteButton() {
   document.getElementById('mute-button')?.addEventListener('click', (e) => {
     (e.target as HTMLElement).blur();
     if (ac.state === 'running') {
-      void ac.suspend().then(function() {
+      void ac.suspend().then(function () {
         (e.target as HTMLElement).textContent = 'Unmute';
       });
     } else if (ac.state === 'suspended') {
-      void ac.resume().then(function() {
+      void ac.resume().then(function () {
         playMusic();
         (e.target as HTMLElement).textContent = 'Mute';
       });
@@ -70,9 +70,7 @@ export function initMuteButton() {
 }
 
 export function updateLevelDisplay(currentLevel: number) {
-  document
-    .querySelector('.level')!
-    .textContent = currentLevel.toString();
+  document.querySelector('.level')!.textContent = currentLevel.toString();
 }
 
 export function updateScore(combo: number) {
@@ -83,14 +81,9 @@ export function updateScore(combo: number) {
 
   SCORE.value += combo * 50 * fpsMultiplier;
 
-  document
-    .querySelector('.score')!
-    .textContent = SCORE.value.toString();
+  document.querySelector('.score')!.textContent = SCORE.value.toString();
 }
 
 export function updateLives() {
-  document
-    .querySelector('.lives')!
-    .textContent = `${LIVES.value - 1}`;
+  document.querySelector('.lives')!.textContent = `${LIVES.value - 1}`;
 }
-
